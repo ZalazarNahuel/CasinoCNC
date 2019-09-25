e08cd676728c58f261a56bbd387c78aaee9aad43
ZalazarNahuel <nahuel210@hotmail.com>
2019-09-25 12:03:33 -0300

    prueba 3

--- javafx/Casino Javafx/src/Main.java
+++ javafx/Casino Javafx/src/Main.java
@@ -1,10 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -22,44 +22,134 @@ public class Main extends Application {
         window = primaryStage;
         window.show();
         window.setTitle("CASINO CNC");
-
-        window.setScene(menuPrincial());
+        menuPrincial();
     }
-    private Scene menuPrincial(){
-        // BOTONES
+    private void closeApp(){
+        window.close();
+    }
+    private void menuPrincial(){
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(tituloMenuPrincipal(),btnesMenuPrincipal());
+        Scene menuPrincipal  = new Scene(root,650,550);
+
+        window.setScene(menuPrincipal);
+    }
+
+    private void menuGenerala(){
+
+        BorderPane root = new BorderPane();
+        //root.getChildren().addAll(textMenuGenerala(),btnesMenuGenerala(),btnBackMenuGenerala());
+        root.setCenter(btnesMenuGenerala());
+        root.setLeft(btnBackMenuGenerala());
+        root.setTop(textMenuGenerala());
+        Scene menuGenerala = new Scene(root,650,550);
+
+        window.setScene(menuGenerala);
+    }
+    private void menuBlackjack(){
+        //TITULO
+        Label titulo = new Label();
+        titulo.setAlignment(Pos.TOP_LEFT);
+        titulo.setText("BLACKJACK");
+        titulo.setTextFill(Color.web("#E60026"));
+        titulo.setFont(new Font("arial",30));
+
+        VBox panelT = new VBox(titulo);
+        panelT.setAlignment(Pos.TOP_CENTER);
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(panelT);
+        Scene menuBlackjack = new Scene(root,650,550);
+        window.setScene(menuBlackjack);
+    }
+    private void jugarGenerala(int cantJugadores){
+        StackPane root = new StackPane();
+        root.getChildren().addAll(tituloMenuGenerala());
+        Scene jugarGenerala = new Scene(root,650,550);
+
+        window.setScene(jugarGenerala);
+    }
+    private VBox tituloMenuPrincipal(){
+        Label titulo = new Label();
+        titulo.setText("CASINO CNC");
+        titulo.setTextFill(Color.web("#E60026"));
+        titulo.setFont(new Font("arial",30));
+
+        VBox panelV = new VBox(titulo);
+        panelV.setAlignment(Pos.TOP_CENTER);
+        return panelV;
+    }
+    private VBox btnesMenuPrincipal(){
         Button btnGenerala = new Button("GENERALA");
         btnGenerala.setOnAction(event -> menuGenerala());
         btnGenerala.setMinSize(150,50);
         Button btnBlackjack = new Button("BLACKJACK");
+        btnBlackjack.setOnAction(event -> menuBlackjack());
         btnBlackjack.setMinSize(150,50);
         Button btnExit = new Button("EXIT");
         btnExit.setOnAction(event -> closeApp());
         btnExit.setMinSize(150,50);
 
-        //TITULO
-        Label titulo = new Label();
-        titulo.setAlignment(Pos.TOP_LEFT);
-        titulo.setText("CASINO CNC");
+        VBox panelV = new VBox(btnGenerala,btnBlackjack,btnExit);
+        panelV.setAlignment(Pos.CENTER);
+        panelV.setSpacing(20);
+
+        return panelV;
+    }
+    private VBox tituloMenuGenerala(){
+
+        Label titulo = new Label("GENERALA");
         titulo.setTextFill(Color.web("#E60026"));
         titulo.setFont(new Font("arial",30));
 
+        VBox panelV = new VBox(titulo);
+        panelV.setAlignment(Pos.TOP_CENTER);
 
-        VBox panelT = new VBox(titulo);
-        panelT.setAlignment(Pos.TOP_CENTER);
-
-        VBox panelB = new VBox(btnGenerala,btnBlackjack,btnExit);
-        panelB.setAlignment(Pos.CENTER);
-        panelB.setSpacing(20);
-
-        StackPane root = new StackPane();
-        root.getChildren().addAll(panelT,panelB);
-        Scene scene  = new Scene(root,650,550);
-        return scene;
+        return panelV;
     }
-    private void closeApp(){
-        window.close();
-    }
-    private void menuGenerala(){
+    private VBox textMenuGenerala(){
 
+        Label texto = new Label("¿Cuantas personas van a jugar?");
+        texto.setFont(new Font("arial",14));
+
+        VBox panelV = new VBox(tituloMenuGenerala(),texto);
+        panelV.setSpacing(100);
+        panelV.setAlignment(Pos.TOP_CENTER);
+
+        return panelV;
     }
+    private VBox btnesMenuGenerala(){
+        Button btnDos = new Button("2");
+        btnDos.setOnAction(event -> jugarGenerala(2));
+        btnDos.setPrefSize(150,50);
+        Button btnTres = new Button("3");
+        btnTres.setOnAction(event -> jugarGenerala(3));
+        btnTres.setPrefSize(150,50);
+        Button btnCuatro = new Button("4");
+        btnCuatro.setOnAction(event -> jugarGenerala(4));
+        btnCuatro.setPrefSize(150,50);
+
+        VBox panelV = new VBox(btnDos,btnTres,btnCuatro);
+        panelV.setAlignment(Pos.CENTER);
+        panelV.setSpacing(20);
+
+
+        return panelV;
+    }
+    private GridPane btnBackMenuGenerala(){
+        Button btnBack = new Button("<- BACK");
+        btnBack.setOnAction(event -> menuPrincial());
+        btnBack.setPrefSize(100,50);
+
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(10,10,10,10));
+        pane.setConstraints(btnBack,0,1);
+        pane.getChildren().addAll(btnBack);
+        pane.setAlignment(Pos.BOTTOM_LEFT);
+
+        return pane;
+    }
+
+
 }
